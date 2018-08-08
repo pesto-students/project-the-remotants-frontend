253a7a15af1876747e67355e05ffd285324d6fa0
Pesto <pesto@ACA801E7.ipt.aol.com>
2018-08-08 11:05:45 +0530

    [Test]: Added end to end test for testing registration flow

---
+++ src/__tests__/endtoend.spec.js
@@ -0,0 +1,31 @@
+import puppeteer from 'puppeteer';
+import { Signup } from '../helpers/endToendTestHandlers';
+
+jest.setTimeout(30 * 1000);
+
+const randomUser = {
+  email: `xyz${Math.random() * 10000}@gmail.com`,
+  password: 'secret',
+};
+
+
+describe('authentication tests', async () => {
+  let page;
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    page = await browser.newPage();
+  });
+
+  describe('Test Sign up', async () => {
+    await test('User should be able to sign up', async () => {
+      Signup(page, randomUser);
+    });
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
+
